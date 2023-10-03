781b3874765facfcf208ac66d5c12f1cd592f859
ajit <hunjan911007@gmail.com>
2023-10-03 15:07:42 +0100

    second commit

---
+++ vars/NexusArtifactUploader.groovy
@@ -0,0 +1,8 @@
+def call(Map<String, String> config, String artifactFilePath) {
+        def credentialsId = 'nexus-credentials',
+        def groupId = 'in.ashokit',
+        def nexusUrl = '13.41.158.5:8081',
+        def nexusVersion = 'nexus3',
+        def protocol = 'http',
+        def repository = 'http://13.41.158.5:8081/repository/ajit-snapshot/',
+        def version = '3.0-SNAPSHOT'
