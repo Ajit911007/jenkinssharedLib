0a436fe75beaad5bd3039b8bd27e1869dda1f52f
Ajit911007 <133755210+Ajit911007@users.noreply.github.com>
2023-10-06 10:58:33 +0100

    Update NexusArtifactUploader.groovy

--- vars/NexusArtifactUploader.groovy
+++ vars/NexusArtifactUploader.groovy
@@ -1,18 +1,17 @@
 def call() {
-    nexusArtifactUploader (
-        artifacts: [
-            [
-                artifactId: '01-maven-web-app',
-                classifier: '',
-                file: 'target/01-maven-web-app.war',
-                type: 'war'
-            ]
-        ],
-        credentialsId: 'nexus-credentials',
-        groupId: 'in.ashokit',
-        nexusUrl: 'http://13.41.240.110:8081/repository/ajit-snapshot/',
-        protocol: 'http',
-        repository: 'ajit-snapshot',
-        version: '3.0-SNAPSHOT'
+    nexusArtifactUploader artifacts: [	
+			[
+				artifactId: '01-maven-web-app',
+				classifier: '',
+				file: 'target/01-maven-web-app.war',
+				type: war		
+			]	
+		],
+		credentialsId: 'nexus-credentials',
+		groupId: 'in.ashokit',
+		nexusUrl: 'http://13.41.240.110:8081/repository/ajit-snapshot/',
+		protocol: 'http',
+		repository: 'ashokit-release'
+		version: '3.0-SNAPSHOT'
     )
 }
